956074c1e1fe4856fc14deb87d4ca8720a94c200
Elisangela Vieira <elyvieira95@gmail.com>
2025-08-23 08:52:51 -0300

    pronto

---
+++ assets/js/scripts.js
@@ -0,0 +1,60 @@
+
+// Menu Navbar (sanduíche)
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu)
+        this.navList = document.querySelector(navList)
+        this.navLinks = document.querySelectorAll(navLinks)
+        this.activeClass = "active"
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation ? (link.style.animation = "") : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
+        })
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass)
+        this.mobileMenu.classList.toggle(this.activeClass)
+        this.animateLinks()
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        console.log(this.navList);
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
+
+
+// Modo Claro/Escuro
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement
+function changeTheme() {
+    const currentTheme = rootHtml.getAttribute("data-theme")
+
+    // usando operador ternário
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+
+    toggleTheme.classList.toggle("bi-sun")
+    toggleTheme.classList.toggle("bi-moon-stars")
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+
